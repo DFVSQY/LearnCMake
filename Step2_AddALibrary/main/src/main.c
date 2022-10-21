699ee0c7592e2ae64c8a69dbc75801340674ccce
DFVSQY <dfvsqy@qq.com>
2022-10-22 03:31:11 +0800

    创建并使用动态库

---
+++ Step2_AddALibrary/main/src/main.c
@@ -0,0 +1,15 @@
+#include "my_math.h"
+#include <stdio.h>
+
+int main() {
+    printf("visual studio code\n");
+
+    int a = 10, b = 20;
+    int c = add_func(a, b);
+    int d = sub_func(a, b);
+    printf("c:%d d:%d\n", c, d);
+
+    printf("hello world\n");
+
+    return 0;
+}
