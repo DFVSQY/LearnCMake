eabaf1e1e05c39094b34b80cf9ee6472bf248db1
DFVSQY <dfvsqy@qq.com>
2022-10-22 14:29:00 +0800

    增加是否使用动态库的开关选项

--- Step2_AddALibrary/main/src/main.c
+++ Step2_AddALibrary/main/src/main.c
@@ -1,12 +1,20 @@
+#include "AddALibrary.h"
+#ifdef USE_MYMATH
 #include "my_math.h"
+#endif
 #include <stdio.h>
 
 int main() {
     printf("visual studio code\n");
 
     int a = 10, b = 20;
+#ifdef USE_MYMATH
     int c = add_func(a, b);
     int d = sub_func(a, b);
+#else
+    int c = 0;// a + b;
+    int d = 0;// a - b;
+#endif
     printf("c:%d d:%d\n", c, d);
 
     printf("hello world\n");
